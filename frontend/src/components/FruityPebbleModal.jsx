44ae5de54d92d4feb6482aa31bb6f46dc96d14bc
DaviSFS21 <davisfs2110@gmail.com>
2025-01-07 22:25:29 -0300

    feat: start POST for pebbles creation

--- frontend/src/components/FruityPebbleModal.jsx
+++ frontend/src/components/FruityPebbleModal.jsx
@@ -1,25 +1,77 @@
+import { useState } from 'react';
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import PropTypes from 'prop-types';
+import Alert from 'react-bootstrap/Alert';
 
-const FruityPebbleModal = ({ show, handleClose }) => {
+const FruityPebbleModal = ({ show, handleClose, CocoaPuffID }) => {
+	const [name, setName] = useState("");
+  const [pebbleCount, setPebbleCount] = useState(0);
+  const [allowSubmit, setAllowSubmit] = useState(false);
+
+  const handleCreate = (e) => {
+		e.preventDefault();
+	
+		fetch(`http://localhost:3000/api/cocoa_puffs/${CocoaPuffID}/fruity_pebbles`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ name, pebble_count: pebbleCount, cocoa_puff_id: CocoaPuffID }),
+		})
+			.then((response) => {
+				if (!response.ok) {
+					return response.json().then((err) => {
+						throw new Error(err.message || "Erro ao criar Fruity Pebble");
+					});
+				}
+				return response.json();
+			})
+			.then((data) => {
+				console.log("Fruity Pebble criado:", data);
+
+				window.location.reload();
+			})
+			.catch((error) => {
+				console.error("Erro ao criar Fruity Pebble:", error);
+			});
+	};
+
+  const checkCount = (event) => {
+    const value = parseInt(event.target.value, 10);
+    setPebbleCount(value);
+    setAllowSubmit(value >= 10);
+  }
+
   return <Modal centered show={show} onHide={handleClose}>
     <Modal.Header>
-      <Modal.Title>Create Fruity Pebble in (CocoaPuffName)</Modal.Title>
+      <Modal.Title>Create Fruity Pebble</Modal.Title>
     </Modal.Header>
 
     <Modal.Body>
-      <Form style={{ display: "grid", gap: "12px" }}>
-        <Form.Control type="text" placeholder="Type Fruity Pebble name" />
-        <Form.Control type="number" placeholder="Type the quantity, less than 10" max="10" />
+      <Form onSubmit={handleCreate} style={{ display: "grid", gap: "12px" }}>
+        <Form.Control
+        type="text"
+        placeholder="Type Fruity Pebble name"
+        onChange={(e) => setName(e.target.value)}
+        />
+        <Form.Control
+        type="number"
+        placeholder="Type the pebble count, less than 10"
+        max="10"
+        onChange={checkCount}
+        />
         <div style={{ display: "flex", gap: "12px" }}>
           <Button variant="danger" onClick={handleClose}>
             Cancel
           </Button>
-          <Button variant="info" type="submit" onClick={handleClose}>
+          <Button disabled={allowSubmit} variant="info" type="submit" onClick={handleClose}>
             Add Fruity Pebble
           </Button>
+          <Alert variant="warning" show={allowSubmit}>
+            Count might be less than 10
+          </Alert>
         </div>
       </Form>
     </Modal.Body>
@@ -28,7 +80,8 @@ const FruityPebbleModal = ({ show, handleClose }) => {
 
 FruityPebbleModal.propTypes = {
   show: PropTypes.bool.isRequired,
-  handleClose: PropTypes.func.isRequired
+  handleClose: PropTypes.func.isRequired,
+  CocoaPuffID: PropTypes.number.isRequired
 };
 
 export default FruityPebbleModal;
