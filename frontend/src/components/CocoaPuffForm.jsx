d78e9bcee7ec3cdeeab37e37f9b69b9e86ac5c21
DaviSFS21 <davisfs2110@gmail.com>
2025-01-07 22:24:13 -0300

    fix: correct alert when create returns an error

--- frontend/src/components/CocoaPuffForm.jsx
+++ frontend/src/components/CocoaPuffForm.jsx
@@ -2,33 +2,47 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Alert from 'react-bootstrap/Alert'
 import { useState } from 'react';
 
 function CocoaPuffForm() {
 	const [name, setName] = useState("");
+	const [alertState, setAlertState] = useState(false);
+	const [alertMessage, setAlertMessage] = useState("");
 
 	const handleCreate = (e) => {
-    e.preventDefault();
+		e.preventDefault();
+	
+		fetch("http://localhost:3000/api/cocoa_puffs", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ name, archived: false }),
+		})
+			.then((response) => {
+				if (!response.ok) {
+					// Lança um erro para respostas com status HTTP fora da faixa 2xx
+					return response.json().then((err) => {
+						throw new Error(err.message || "Erro ao criar CocoaPuff");
+					});
+				}
+				return response.json();
+			})
+			.then((data) => {
+				console.log("CocoaPuff criado:", data);
 
-    fetch("http://localhost:3000/api/cocoa_puffs", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(
-        { name, archived: false },
-      ),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("CocoaPuff criado:", data);
-        alert("CocoaPuff criado com sucesso!");
-      })
-      .catch((error) => {
-        console.error("Erro ao criar CocoaPuff:", error);
-        alert("Erro ao criar CocoaPuff");
-      });
-  };
+				window.location.reload();
+			})
+			.catch((error) => {
+				console.error("Erro ao criar CocoaPuff:", error);
+				setAlertMessage(error.message || "Erro ao criar CocoaPuff");
+				setAlertState(true);
+				setTimeout(() => {
+					setAlertState(false);
+				}, 3000);
+			});
+	};	
 	
   return (
 		<Form onSubmit={handleCreate} style={{ display: "flex", gap: "12px" }}>
@@ -48,7 +62,9 @@ function CocoaPuffForm() {
 					</Button>
 				</Col>
 				<Col>
-					
+					<Alert key="warning" variant="warning" show={alertState}>
+						{alertMessage}
+					</Alert>
 				</Col>
 			</Row>
 		</Form>
